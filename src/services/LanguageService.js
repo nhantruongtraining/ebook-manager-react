906adfbf8de49f118eb4d0b93ba3409a18228e88
nhantruongtraining <nhan.truongcongthu@axonactive.com>
2022-07-11 18:33:42 +0700

    start

---
+++ src/services/LanguageService.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const LANGUAGE_API_BASE_URL = "http://localhost:8080/api/languages";
+
+class Language {
+    getLanguages() {
+        return axios.get(LANGUAGE_API_BASE_URL);
+    }
+
+    createLanguage(language) {
+        return axios.post(LANGUAGE_API_BASE_URL, language);
+    }
+
+    getLanguageById(languageId) {
+        return axios.get(LANGUAGE_API_BASE_URL + '/' + languageId);
+    }
+
+    updateLanguageById(language, languageId) {
+        return axios.put(LANGUAGE_API_BASE_URL + '/' + language, languageId);
+    }
+
+    deleteLanguageById(languageId) {
+        return axios.delete(LANGUAGE_API_BASE_URL + '/' + languageId);
+    }
+}
+
+export default new LanguageService();
