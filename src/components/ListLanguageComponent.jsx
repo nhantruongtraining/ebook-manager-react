0418d660e04899d7c8d1abb20d37eb4b17f61719
nhantruongtraining <nhan.truongcongthu@axonactive.com>
2022-07-13 13:26:23 +0700

    remove import useNavigate

--- src/components/ListLanguageComponent.jsx
+++ src/components/ListLanguageComponent.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import LanguageService from "../services/LanguageService";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 class ListLanguageComponent extends Component {
   constructor(props) {
